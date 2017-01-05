56cecd32427c4bf47ee2293e127262f07f0cf504
BorisMansencal <boris.mansencal@labri.fr>
2017-01-05 18:32:59 +0100

    update to last version of binarization in Document Creator

--- server/headers/binarization.hpp
+++ server/headers/binarization.hpp
@@ -1,34 +1,40 @@
 #ifndef BINARIZATION_H
 #define BINARIZATION_H
 
-/**
- * \file Binarization.hpp
- * \brief Binarization operations
- * \author provided by Antoine BILLY
- * \version 1.0
- * \date Mars 2016
- *
- * Operations for binarize a image
- *
- */
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
 
-enum NiblackVersion 
+class Binarization
 {
-	NIBLACK=0,
-    SAUVOLA,
-    WOLFJOLION,
+
+public:
+
+  enum NiblackVersion
+    {
+      NIBLACK=0,
+      SAUVOLA,
+      WOLFJOLION,
+    };
+
+  static double binarize(const cv::Mat &src, cv::Mat &dst, int method=CV_THRESH_OTSU, int thresholdType=17, int blockSize=11);
+  static void preProcess(const cv::Mat &src, cv::Mat &dst, int erosion=12);
+  static void postProcess(const cv::Mat &src, cv::Mat &dst, double v=0.9, int win=7);
+
+  static void NiblackSauvolaWolfJolion(const cv::Mat &im, cv::Mat &output, NiblackVersion version, double dR=128, int winx=40, int winy=40, double k=0.34);
+
+  static void applyErosion(const cv::Mat &src, cv::Mat &dst, int erosion_type=2, int erosion_size=2);
+  static void applyDilation(const cv::Mat &src, cv::Mat &dst, int dilation_type=2, int dilation_size=2);
+
+  static void shrinkFilter(const cv::Mat &src, cv::Mat &dst, double v=0.9, int win=5);
+  static void swellFilter(const cv::Mat &src, cv::Mat &dst, int win=16);
+  static void connectivityFilter(const cv::Mat &src, cv::Mat &dst);
+
+protected:
+
+  static double calcLocalStats(const cv::Mat &im, cv::Mat &map_m, cv::Mat &map_s, int winx, int winy);
+
+
 };
-
-#define uget(x,y)    at<unsigned char>(y,x)
-#define uset(x,y,v)  at<unsigned char>(y,x)=v;
-#define fget(x,y)    at<float>(y,x)
-#define fset(x,y,v)  at<float>(y,x)=v;
-
-double calcLocalStats (cv::Mat &im, cv::Mat &map_m, cv::Mat &map_s, int winx, int winy);
-void NiblackSauvolaWolfJolion (cv::Mat im, cv::Mat output, NiblackVersion version, double dR=128, int winx=40, int winy=40, double k=0.34);
 
 #endif // BINARIZATION_H
