6363134ad3fc776c2d1be6d60bf7bd8a80a1d8ab
BorisMansencal <boris.mansencal@labri.fr>
2017-01-05 18:35:50 +0100

    indent

--- server/src/Character.cpp
+++ server/src/Character.cpp
@@ -3,47 +3,53 @@
 
 Character::Character():m_baseline(0)
 {}
-Character::Character(const std::string &l):m_label(l), m_baseline(0)
+
+Character::Character(const std::string &l):
+  m_label(l), m_baseline(0)
 {}
 
 
 std::string Character::getLabel() const
 {
-	return m_label;
+  return m_label;
 }
 
 void Character::setBaseline(int x)
 {
-	m_baseline = x;
+  m_baseline = x;
 }
 
 int Character::getBaseline() const
 {
-	return m_baseline;
+  return m_baseline;
 }
 
 int Character::countComponent() const
 {
-	return m_idComponentsConnexes.size();
+  return m_idComponentsConnexes.size();
 }
 void Character::addComponent(int idCC, int idLine)
 {
-	if(!hasComponent(idCC, idLine))
-		m_idComponentsConnexes.push_back(std::make_pair(idLine,idCC));
+  if(!hasComponent(idCC, idLine))
+    m_idComponentsConnexes.push_back(std::make_pair(idLine,idCC));
 }
+
 void Character::removeComponent(int idCC, int idLine)
 {
-	if(hasComponent(idCC, idLine))
-		m_idComponentsConnexes.erase(std::remove(m_idComponentsConnexes.begin(), m_idComponentsConnexes.end(), std::make_pair(idLine,idCC)), m_idComponentsConnexes.end()); 
+  if(hasComponent(idCC, idLine))
+    m_idComponentsConnexes.erase(std::remove(m_idComponentsConnexes.begin(), m_idComponentsConnexes.end(), std::make_pair(idLine,idCC)), m_idComponentsConnexes.end()); 
 }
+
 bool Character::hasComponent(int idCC, int idLine) const
 {
-	for (const auto &s : m_idComponentsConnexes) 
-		if(s.first == idLine && s.second == idCC)
-			return true;
-		return false;
+  for (const auto &s : m_idComponentsConnexes) {
+    if(s.first == idLine && s.second == idCC)
+      return true;
+  }
+  return false;
 
 }
+
 std::pair<int,int> Character::getIdComponentAtIndex(int index) const
 {
   assert(index>=0 && size_t(index)<=m_idComponentsConnexes.size());
@@ -52,5 +58,5 @@ std::pair<int,int> Character::getIdComponentAtIndex(int index) const
 
 bool operator== ( const Character &c1, const Character &c2) 
 {
-        return c1.m_label == c2.m_label;
+  return c1.m_label == c2.m_label;
 }
