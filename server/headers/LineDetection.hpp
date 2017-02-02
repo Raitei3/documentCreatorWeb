3eb57bfb76393fa55f6909ce711ecea5ceeed7b4
Antoine <antoine33.borde@gmail.com>
2017-02-02 17:49:27 +0100

    Portage qui compile de LineDetection. On ne peut pas test tout de suite si ça marche

---
+++ server/headers/LineDetection.hpp
@@ -0,0 +1,38 @@
+#ifndef LINEDETECTIONDIALOG_HPP
+#define LINEDETECTIONDIALOG_HPP
+
+/*#include <QImage>
+#include <QObject>*/
+#include <QDialog>
+
+#include "opencv2/core/core.hpp"
+
+
+class QLabel;
+
+class LineDetectionDialog : public QObject
+{
+    //Q_OBJECT
+
+public:
+    explicit LineDetectionDialog(QObject *parent = 0);
+    ~LineDetectionDialog();
+    void setOriginalImage(const QImage &img);
+    QImage getResultImage() { return _houghImg; }
+
+protected:
+    void process();
+    int getCharacterHeight(cv::Mat img);
+    void drawBaseLines(const cv::Mat &img, cv::Mat &dst, const std::vector<cv::Vec4i> &lines);
+
+private:
+    QLabel *_thumbnail;
+    QLabel *_originalLabel;
+
+    QImage _originalImg;
+    QImage _houghImg;
+
+    int _character_height;
+};
+
+#endif // LINEDETECTIONDIALOG_HPP
