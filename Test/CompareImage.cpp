3fbac27c0a5b10a3739fe81e6e4256eb5cc0eeaa
Antoine <antoine33.borde@gmail.com>
2017-03-15 19:35:17 +0100

    amélioration du programme de comparaison de deux image

--- Test/CompareImage.cpp
+++ Test/CompareImage.cpp
@@ -6,7 +6,7 @@
 using namespace cv;
 using namespace std;
 
-float CompareImage(string image1 , string image2)
+float CompareImage(string image1 , string image2, string savePath)
 {
   Mat gray1,gray2;
 cv::Mat img1 = cv::imread(image1);
@@ -20,6 +20,7 @@ float nbPixels = size.height*size.width;
 
 Mat result,gray;
 cv::compare(img1 , img2  , result , cv::CMP_EQ );
+cv::imwrite(savePath,result);
 cvtColor(result, gray, CV_BGR2GRAY);
 float similarPixels  = countNonZero(gray);
 float res = (similarPixels/nbPixels)*100;
@@ -29,11 +30,15 @@ std::cout << "corresponding to "<<res<<"%" << std::endl;
 
 int main(int argc,char* argv[])
 {
-  if (argc!=3) {
+  if (!((argc == 3)||(argc==4))) {
     std::cout << "/* ./CompareImage Image1 Image2 */" << std::endl;
   }
-string s1 = argv[1];
-string s2 = argv[2];
-CompareImage(s1,s2);
+  string s1 = argv[1];
+  string s2 = argv[2];
+  if (argc == 4) {
+    CompareImage(s1,s2,argv[3]);
+  }
+  else
+  CompareImage(s1,s2,"images/resultCompareImage.png");
 return 1;
 }
