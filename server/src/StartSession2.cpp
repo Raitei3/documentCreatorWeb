8c7e3ccdfe9834479e46c0382c5a597c1c1d13b4
Paul BEZIAU <paul.beziau@laposte.net>
2017-03-31 16:57:51 +0200

    correction d'un bug rare supprimant une image du serveur

--- server/src/StartSession2.cpp
+++ server/src/StartSession2.cpp
@@ -1,13 +1,23 @@
 #include "StartSession2.hpp"
 #include "util.hpp"
+
+using namespace std;
+
 bool StartSession2::getPage(HttpRequest* request, HttpResponse *response)
 {
-    if(request->getRequestType()==POST_METHOD){
-        return _startSession.getPage(request,response);
-    }
-    else{
-        std::string json_Session = InitiateSession("../../server/data/background/newBackGround2.png", request);
-        NVJ_LOG->append(NVJ_ERROR, "Start Session : " + json_Session);
-        return fromString(json_Session, response);
-    }
+  if(request->getRequestType()==POST_METHOD){
+    return _startSession.getPage(request,response);
+  }
+  else{
+    string nameCopy=gen_random(".png");
+    ifstream orig("data/background/newBackGround2.png");
+    ofstream dest(UPLOAD_DIR + nameCopy);
+    cerr<<nameCopy<<endl;
+    dest<<orig.rdbuf();
+    orig.close();
+    dest.close();
+    string json_Session = InitiateSession(nameCopy, request);
+    NVJ_LOG->append(NVJ_ERROR, "Start Session : " + json_Session);
+    return fromString(json_Session, response);
+  }
 }
